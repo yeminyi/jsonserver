bdf78235b1553327a50f23d3cf1f1c3fa0a11a4e
Amy <yeminyi@outlook.com>
2019-08-17 00:17:48 +1000

    new for task firebase

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require('firebase-functions');
-const pluralize = require('pluralize')
+// const pluralize = require('pluralize')
 const jsonServer = require('json-server')
 
 const main = jsonServer.create()
 const api = jsonServer.create()
-const router = jsonServer.router('./tmp/db.json', { foreignKeySuffix: 'Id' })
+const router = jsonServer.router('/tmp/db.json', { foreignKeySuffix: 'Id' })
 const middlewares = jsonServer.defaults()
 
 router.render = function (req, res) {
